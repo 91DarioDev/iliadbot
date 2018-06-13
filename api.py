81150407bdc87c634f528b288357c14cb3962809
91DarioDev <dariomsn@hotmail.it>
2018-06-13 13:28:48 +0200

    api fix

--- api.py
+++ api.py
@@ -4,47 +4,47 @@ from lxml import html
 url = "https://www.iliad.it/account/"
 
 dic = {
-	'chiamate_tot' : '//*[@id="page-container"]/div/div[2]/div[2]/div[1]/div[1]/div[1]/div/div[1]/span[1]/text()',
-	'gb_tot' : '//*[@id="page-container"]/div/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[1]/span[1]/text()',
-	'sms_tot' : '//*[@id="page-container"]/div/div[2]/div[2]/div[1]/div[1]/div[2]/div/div[1]/span[1]/text()',
-	'mms_tot' : '//*[@id="page-container"]/div/div[2]/div[2]/div[1]/div[2]/div[2]/div/div[1]/span[1]/text()',
-	'consumo_tot' : '//*[@id="page-container"]/div/div[2]/div[2]/div[5]/div[2]/text()',
-	'credito_residuo' : '//*[@id="page-container"]/div/div[2]/div[2]/div[5]/div[4]/text()',
+    'chiamate_tot' : '//*[@id="page-container"]/div/div[2]/div[2]/div[1]/div[1]/div[1]/div/div[1]/span[1]/text()',
+    'gb_tot' : '//*[@id="page-container"]/div/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[1]/span[1]/text()',
+    'sms_tot' : '//*[@id="page-container"]/div/div[2]/div[2]/div[1]/div[1]/div[2]/div/div[1]/span[1]/text()',
+    'mms_tot' : '//*[@id="page-container"]/div/div[2]/div[2]/div[1]/div[2]/div[2]/div/div[1]/span[1]/text()',
+    'consumo_tot' : '//*[@id="page-container"]/div/div[2]/div[2]/div[5]/div[2]/text()',
+    'credito_residuo' : '//*[@id="page-container"]/div/div[2]/div[2]/div[5]/div[4]/text()',
 }
 
 errors = {
-	'credentials': '//*[@id="page-container"]/div/div[1]/div/text()'
+    'credentials': '//*[@id="page-container"]/div/div[1]/div/text()'
 }
 
 def login(id, pwd):
-	"""
-	params: id, pwd
-	return: TreeObject
-	"""
-	data = {"login-ident":id, "login-pwd":pwd}
+    """
+    params: id, pwd
+    return: TreeObject
+    """
+    data = {"login-ident":id, "login-pwd":pwd}
 
-	r = requests.post(url, data=data)
-	tree = html.fromstring(r.content)
+    r = requests.post(url, data=data)
+    tree = html.fromstring(r.content)
 
-	return tree
+    return tree
 
 def get_info(tree):
-	"""
-	Parse iliad info from tree object of the html profile page
-	
-	params: TreeObject
-	return: dic
-	"""
-	info = {'ok':{}, 'error':False}
+    """
+    Parse iliad info from tree object of the html profile page
+    
+    params: TreeObject
+    return: dic
+    """
+    info = {'ok':{}, 'error':False}
 
-	for err_name, xpath in errors.items():
-		error = tree.xpath(xpath)
-		if error:
-			info['error'] = err_name
-			return info
+    for err_name, xpath in errors.items():
+        error = tree.xpath(xpath)
+        if error:
+            info['error'] = err_name
+            return info
 
-	for k, v in dic.items():
-		res = tree.xpath(v)
-		info['ok'].update({k:res[0]})
-
-	return info
+    for k, v in dic.items():
+        res = tree.xpath(v)
+        if res:
+          info['ok'].update({k:res[0]})
+    return info
