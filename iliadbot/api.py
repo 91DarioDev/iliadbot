987ffac467b5de06215d58887b05b79ed2dc5132
91DarioDev <dariomsn@hotmail.it>
2018-07-02 17:30:04 +0200

    fix html parsing

--- iliadbot/api.py
+++ iliadbot/api.py
@@ -25,12 +25,12 @@ from iliadbot import emoji
 url = "https://www.iliad.it/account/"
 
 dic = collections.OrderedDict()
-dic["{} chiamate".format(emoji.telephone)] = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[1]/div[1]/div[1]/div/div[1]"
-dic["{} sms".format(emoji.sms)] = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[1]/div[1]/div[2]/div/div[1]"
-dic["{} internet".format(emoji.internet)] = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[1]"
-dic["{} mms".format(emoji.mms)] = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[1]/div[2]/div[2]/div/div[1]"
-dic["{} consumo totale".format(emoji.money)] = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[5]/div[2]"
-dic["{} credito residuo".format(emoji.money)] = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[5]/div[4]"
+dic["{} chiamate".format(emoji.telephone)] = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/div[1]"
+dic["{} sms".format(emoji.sms)] = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[2]/div/div[1]"
+dic["{} internet".format(emoji.internet)] = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div[1]"
+dic["{} mms".format(emoji.mms)] = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[2]/div[2]/div[2]/div/div[1]"
+dic["{} consumo totale".format(emoji.money)] = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[6]/div[2]"
+dic["{} credito residuo".format(emoji.money)] = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[6]/div[4]"
 
 errors = {
     'credentials': '//*[@id="page-container"]/div/div[1]/div/text()'
