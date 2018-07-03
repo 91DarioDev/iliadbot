dc0b079c650d6ab387f5c1591c390a836c456b6d
91DarioDev <dariomsn@hotmail.it>
2018-07-03 19:51:33 +0200

    fix double chiamate

--- iliadbot/utils.py
+++ iliadbot/utils.py
@@ -28,6 +28,8 @@ def adjust_parsed_info(dic):
 
         elif i[0] == "{} rinnovo".format(emoji.renewal):
             i[1] = i[1].split("alle ")[1]
+
+        elif i[0] == "{} chiamate".format(emoji.telephone):
+            i[1] = " ".join(i[1].split(": ")[1:])  # remove double "chiamate"
     
     return dic
-    
