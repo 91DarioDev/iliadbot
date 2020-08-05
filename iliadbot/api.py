cd3d3bdb366fa047e1054134365b8fe6ee13fdf5
Dario <dariomsn@hotmail.it>
2020-08-05 13:14:15 +0200

    fixed

--- iliadbot/api.py
+++ iliadbot/api.py
@@ -26,25 +26,20 @@ url = "https://www.iliad.it/account/"
 
 # italia xpaths
 def get_italia(tree):
-    print(dic_italia["{} chiamate".format(emoji.telephone)] = tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[3]/div[1]/div[1]/div/div[1]/span[1]/text()'),
-    dic_italia["{} sms".format(emoji.sms)] = tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[3]/div[1]/div[2]/div/div[1]/span[1]/text()'),
-    dic_italia["{} internet".format(emoji.internet)] = tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div/div[1]/text()'),
-    dic_italia["{} mms".format(emoji.mms)] =tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[3]/div[2]/div[2]/div/div[1]/span[1]/text()'))
-    
     dic_italia = collections.OrderedDict()
     dic_italia["{} chiamate".format(emoji.telephone)] = tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[3]/div[1]/div[1]/div/div[1]/span[1]/text()')[0]
     dic_italia["{} sms".format(emoji.sms)] = tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[3]/div[1]/div[2]/div/div[1]/span[1]/text()')[0]
-    dic_italia["{} internet".format(emoji.internet)] = tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div/div[1]/text()')[0]
+    dic_italia["{} internet".format(emoji.internet)] = tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div/div[1]/span[1]/text()')[0]
     dic_italia["{} mms".format(emoji.mms)] =tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[3]/div[2]/div[2]/div/div[1]/span[1]/text()')[0]
     return dic_italia
 
 # estero xpaths
 def get_estero(tree):
     dic_estero = collections.OrderedDict()
-    dic_estero["{} chiamate".format(emoji.telephone)] = tree.xpath('//div[@class="conso-infos conso-roaming"]//div[@class="conso__text"]/span[2]/text()')[0]
-    dic_estero["{} sms".format(emoji.sms)] = tree.xpath('//div[@class="conso-infos conso-roaming"]//div[@class="conso__text"]/span[2]/text()')[1]
-    dic_estero["{} internet".format(emoji.internet)] = tree.xpath('//div[@class="conso-infos conso-roaming"]//div[@class="conso__text"]/span[2]/text()')[2]
-    dic_estero["{} mms".format(emoji.mms)] =tree.xpath('//div[@class="conso-infos conso-roaming"]//div[@class="conso__text"]/span[2]/text()')[3]
+    dic_estero["{} chiamate".format(emoji.telephone)] = tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[4]/div[1]/div[1]/div/div[1]/span[1]/text()')[0]
+    dic_estero["{} sms".format(emoji.sms)] = tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[4]/div[1]/div[2]/div/div[1]/span[1]/text()')[0]
+    dic_estero["{} internet".format(emoji.internet)] = tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[1]/span[1]/text()')[0]
+    dic_estero["{} mms".format(emoji.mms)] =tree.xpath('/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div[4]/div[2]/div[2]/div/div[1]/span[1]/text()')[0]
     return dic_estero
 
 def get_general(tree):
